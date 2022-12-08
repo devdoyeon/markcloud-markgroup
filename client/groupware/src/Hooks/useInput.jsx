3670562b558262435ef699a2e03dc9283def1c6f
ByeongUk-An <quddnr005@gmail.com>
2022-12-08 15:16:08 +0900

    front:react initial setting

---
+++ client/groupware/src/Hooks/useInput.jsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const useInput = (initValue) => {
+  // 사용법 const inputAgree = useInput(false);
+  const [value, setValue] = useState(initValue);
+
+  const onChange = (e) => {
+    setValue(e.target.value);
+  };
+
+  return { value, onChange, setValue };
+};
+
+export default useInput;
