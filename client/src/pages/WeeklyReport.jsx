e13a482baa81d9e50b6a29bea93515aca8e09c32
DOYEON-YOU <doyeonyou@naver.com>
2022-12-12 10:11:10 +0900

    modify: Apply WeeklyReport None Style

--- client/src/pages/WeeklyReport.jsx
+++ client/src/pages/WeeklyReport.jsx
@@ -1,10 +1,24 @@
 import SideMenu from 'common/SideMenu';
+import noneImg from 'image/noneList.svg';
 
 const WeeklyReport = () => {
   return (
     <div className='container'>
       <SideMenu />
-      <div className='content-wrap'>WeeklyReport</div>
+    <div className='content-wrap notice'>
+      <div className='header'>
+        <h3>주간 업무 보고</h3>
+      </div>
+      <div className='list-wrap'>
+        <div className='noneList column'>
+          <img src={noneImg} alt='글 없음 아이콘' />
+          <span>등록된 게시글이 없습니다.</span>
+        </div>
+        <div className='btn-wrap'>
+          <button className='commonBtn noticeBtn'>등록</button>
+        </div>
+      </div>
+    </div>
     </div>
   );
 };
