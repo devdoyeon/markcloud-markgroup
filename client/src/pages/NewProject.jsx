a1c9da81811c150869ad76a7b2cedbc6319561b1
DOYEON-YOU <doyeonyou@naver.com>
2022-12-14 13:51:28 +0900

    modify: NewProject Page

--- client/src/pages/NewProject.jsx
+++ client/src/pages/NewProject.jsx
@@ -1,7 +1,10 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import SideMenu from 'common/SideMenu';
 import EditorComponent from 'common/EditorComponent';
 import selectArrow from 'image/selectArrow.svg';
+import deletePerson from 'image/deletePersonIcon.svg';
+import CommonModal from 'common/CommonModal';
+import { commonModalSetting } from 'js/commonUtils';
 
 const NewProject = () => {
   const personArr = [
@@ -12,144 +15,203 @@ const NewProject = () => {
     '정다은',
     '송지은',
   ];
+  const [alertBox, setAlertBox] = useState({
+    mode: '',
+    context: '',
+    bool: false,
+  });
   const [statusSelect, setStatusSelect] = useState('off');
   const [personSelect, setPersonSelect] = useState('off');
   const [statusValue, setStatusValue] = useState('===');
   const [personValue, setPersonValue] = useState('===');
+  const [participation, setParticipation] = useState([]);
+  useEffect(() => {
+    console.log(participation);
+  }, [participation]);
 
   return (
-    <div className='container'>
-      <SideMenu />
-      <div className='content-wrap project'>
-        <div className='header'>
-          <h3>프로젝트 현황</h3>
-        </div>
-        <div className='createProjectWrap'>
-          <div className='projectTitle'>
-            제목
+    <>
+      <div className='container'>
+        <SideMenu />
+        <div className='content-wrap project'>
+          <div className='header'>
+            <h3>프로젝트 현황</h3>
           </div>
-          <div className='projectInfo column'>
-            <hr />
-            <div className='row makeProjectDate'>
-              <span>작성일</span>
-              <input type='text' />
-            </div>
-            <hr />
-            <div className='row date'>
-              <div className='row start'>
-                <span>프로젝트 시작일</span>
-                <input type='date' />
+          <div className='createProjectWrap'>
+            <div className='projectInfo column'>
+              <hr />
+              <div className='row makeProjectDate'>
+                <span>프로젝트명</span>
+                <input type='text' />
               </div>
+              <hr />
               <div className='row date'>
-                <span>프로젝트 종료일</span>
-                <input type='date' />
-              </div>
-            </div>
-            <hr />
-            <div className='row'>
-              <span>참여 인원</span>
-              <div className='row'>
-                <span className='personBtn'>
-                  권도연
-                </span>
-              </div>
-            </div>
-            <hr />
-          </div>
-          <EditorComponent />
-          <div className='projectSetting column'>
-            <div className='row'>
-              <span>프로젝트 상태</span>
-              <div className={`selectBox status ${statusSelect}`}>
-                <div
-                  className={`selectVal`}
-                  onClick={() =>
-                    statusSelect === 'on'
-                      ? setStatusSelect('off')
-                      : setStatusSelect('on')
-                  }>
-                  {statusValue}
-                  <img src={selectArrow} alt='선택 아이콘' />
+                <div className='row start'>
+                  <span>프로젝트 시작일</span>
+                  <input type='date' />
                 </div>
-                {statusSelect === 'on' && (
-                  <div className='selectOptGroup'>
-                    <div
-                      className={`selectOpt ${
-                        statusValue === '시작 전' && 'active'
-                      }`}
-                      onClick={() => {
-                        setStatusValue('시작 전');
-                        setStatusSelect('off');
-                      }}>
-                      시작 전
-                    </div>
-                    <div
-                      className={`selectOpt ${
-                        statusValue === '진행 중' && 'active'
-                      }`}
-                      onClick={() => {
-                        setStatusValue('진행 중');
-                        setStatusSelect('off');
-                      }}>
-                      진행 중
-                    </div>
-                    <div
-                      className={`selectOpt ${
-                        statusValue === '종료' && 'active'
-                      }`}
-                      onClick={() => {
-                        setStatusValue('종료');
-                        setStatusSelect('off');
-                      }}>
-                      종료
-                    </div>
-                  </div>
-                )}
+                <div className='row date'>
+                  <span>프로젝트 종료일</span>
+                  <input type='date' />
+                </div>
               </div>
+              <hr />
+              <div
+                className={`row peopleWrap ${
+                  participation?.length === 0 ? 'nonePeople' : ''
+                }`}>
+                <span>
+                  {participation?.length === 0
+                    ? '참여 인원을 추가해 주세요.'
+                    : '참여 인원 목록'}
+                </span>
+                <div className='row'>
+                  {participation?.reduce((acc, person) => {
+                    return (
+                      <>
+                        {acc}
+                        <span className='personBtn'>
+                          {person}
+                          <img
+                            src={deletePerson}
+                            alt={`${person} 삭제 버튼`}
+                            onClick={() => {
+                              setParticipation(prev => {
+                                const clone = [...prev];
+                                clone.splice(clone.indexOf(person), 1)
+                                return clone;
+                              })
+                            }}
+                          />
+                        </span>
+                      </>
+                    );
+                  }, <></>)}
+                </div>
+              </div>
+              <hr />
             </div>
-            <div className='row'>
-              <span>참여 인원</span>
-              <div className='people column'>
-                <div className={`selectBox status ${personSelect}`}>
+            <EditorComponent />
+            <div className='projectSetting column'>
+              <div className='row'>
+                <span>프로젝트 상태</span>
+                <div className={`selectBox status ${statusSelect}`}>
                   <div
                     className={`selectVal`}
                     onClick={() =>
-                      personSelect === 'on'
-                        ? setPersonSelect('off')
-                        : setPersonSelect('on')
+                      statusSelect === 'on'
+                        ? setStatusSelect('off')
+                        : setStatusSelect('on')
                     }>
-                    {personValue}
+                    {statusValue}
                     <img src={selectArrow} alt='선택 아이콘' />
                   </div>
-                  {personSelect === 'on' && (
+                  {statusSelect === 'on' && (
                     <div className='selectOptGroup'>
-                      {personArr.reduce((acc, person) => {
-                        return (
-                          <>
-                            {acc}
-                            <div
-                              className={`selectOpt ${
-                                personValue === person && 'active'
-                              }`}
-                              onClick={() => {
-                                setPersonValue(person);
-                                setPersonSelect('off');
-                              }}>
-                              {person}
-                            </div>
-                          </>
-                        );
-                      }, <></>)}
+                      <div
+                        className={`selectOpt ${
+                          statusValue === '시작 전' && 'active'
+                        }`}
+                        onClick={() => {
+                          setStatusValue('시작 전');
+                          setStatusSelect('off');
+                        }}>
+                        시작 전
+                      </div>
+                      <div
+                        className={`selectOpt ${
+                          statusValue === '진행 중' && 'active'
+                        }`}
+                        onClick={() => {
+                          setStatusValue('진행 중');
+                          setStatusSelect('off');
+                        }}>
+                        진행 중
+                      </div>
+                      <div
+                        className={`selectOpt ${
+                          statusValue === '종료' && 'active'
+                        }`}
+                        onClick={() => {
+                          setStatusValue('종료');
+                          setStatusSelect('off');
+                        }}>
+                        종료
+                      </div>
                     </div>
                   )}
                 </div>
               </div>
-              <button className='addPersonBtn'>추가</button>
+              <div className='row'>
+                <span>참여 인원 추가</span>
+                <div className='people column'>
+                  <div className={`selectBox status ${personSelect}`}>
+                    <div
+                      className={`selectVal`}
+                      onClick={() =>
+                        personSelect === 'on'
+                          ? setPersonSelect('off')
+                          : setPersonSelect('on')
+                      }>
+                      {personValue}
+                      <img src={selectArrow} alt='선택 아이콘' />
+                    </div>
+                    {personSelect === 'on' && (
+                      <div className='selectOptGroup'>
+                        {personArr.reduce((acc, person) => {
+                          return (
+                            <>
+                              {acc}
+                              <div
+                                className={`selectOpt ${
+                                  personValue === person && 'active'
+                                }`}
+                                onClick={() => {
+                                  setPersonValue(person);
+                                  setPersonSelect('off');
+                                }}>
+                                {person}
+                              </div>
+                            </>
+                          );
+                        }, <></>)}
+                      </div>
+                    )}
+                  </div>
+                </div>
+                <button
+                  className='addPersonBtn'
+                  onClick={() =>
+                    setParticipation(prev => {
+                      const clone = [...prev];
+                      if (clone.includes(personValue)) {
+                        commonModalSetting(
+                          setAlertBox,
+                          true,
+                          'alert',
+                          '이미 추가된 인원입니다.'
+                        );
+                        return clone;
+                      } else clone.push(personValue);
+                      return clone;
+                    })
+                  }>
+                  추가
+                </button>
+              </div>
             </div>
           </div>
         </div>
       </div>
-    </div>
+      {alertBox.bool && (
+        <CommonModal
+          setModal={setAlertBox}
+          modal={alertBox}
+          okFn={() => {}}
+          failFn={() => {}}
+        />
+      )}
+    </>
   );
 };
 
