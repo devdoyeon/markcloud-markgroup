780ac0f7ade2fe525b1cf3bb5e99d581cc1a3efd
Jione Jang <jenny.jione.jang@gmail.com>
2022-12-28 11:41:19 +0900

    HTTPException (AlreadyProjectName)

--- server/crud/project_crud.py
+++ server/crud/project_crud.py
@@ -161,35 +161,35 @@ def get_project_members(db: Session, project_code: str):
 
 
 def create_project(db: Session, user_pk: int, project_create: ProjectCreate):
-    try:
-        user_info = author_chk.get_user_info(db, user_pk)
-        # organ_code = utils.get_organ_code(db, user_id)
-        # 등록 버튼 클릭 시, 프로젝트 등록(groupware_project 테이블)
-        #     프로젝트코드 생성규칙
-        #     PRJ + 년월일 + Random 난수번호(5자리)
-        #     ex) PRJ22121618923
-        yymmdd = datetime.today().strftime("%y%m%d")
-        random_int = random.randint(10000,99999)
-        random_number = str(random_int)
-        project_code = "PRJ" + yymmdd + random_number
-        print(project_code)
-        print(project_create.project_status)
+    
+    # 프로젝트 이름이 이미 있으면 생성하지 못하게 하기
+    input_project_name = project_create.project_name
+    project = db.query(ProjectTable).filter(ProjectTable.project_name == input_project_name).first()
+    if project:
+        raise HTTPException(status_code=403, detail='AlreadyProjectName')
+    
+    user_info = author_chk.get_user_info(db, user_pk)
+    # PRJ + 년월일 + Random 난수번호(5자리)
+    yymmdd = datetime.today().strftime("%y%m%d")
+    random_int = random.randint(10000,99999)
+    random_number = str(random_int)
+    project_code = "PRJ" + yymmdd + random_number
+    print(project_code)
+    print(project_create.project_status)
 
-        db_project = ProjectTable(organ_code=user_info.department_code,
-                                project_code=project_code,
-                                project_name=project_create.project_name,
-                                project_description=project_create.project_description,
-                                project_start_date=project_create.project_start_date,
-                                project_end_date=project_create.project_end_date,
-                                project_status=project_create.project_status,
-                                created_at=datetime.now(),
-                                created_id=user_pk,
-                                updated_at=datetime.now(),
-                                updated_id=user_pk
-                                )
-        db.add(db_project)
-    except:
-        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="DB ERROR")
+    db_project = ProjectTable(organ_code=user_info.department_code,
+                            project_code=project_code,
+                            project_name=project_create.project_name,
+                            project_description=project_create.project_description,
+                            project_start_date=project_create.project_start_date,
+                            project_end_date=project_create.project_end_date,
+                            project_status=project_create.project_status,
+                            created_at=datetime.now(),
+                            created_id=user_pk,
+                            updated_at=datetime.now(),
+                            updated_id=user_pk
+                            )
+    db.add(db_project)
 
     
 def update_project(db: Session, project_update: ProjectUpdate, project_id: int, user_pk: int):
@@ -278,6 +278,12 @@ def delete_project(db: Session, project_id: int, user_pk: int):
     project_code = db_project.project_code
     
     if user_pk == int(db_project.created_id) or user_info.groupware_only_yn == 'N':
+        
+        # groupware_work_management 테이블에 있는 프로젝트는 삭제하면 안됨.
+        q = db.query(ProjectManageTable).filter(ProjectManageTable.project_code == project_code).first()
+        if q:
+            raise HTTPException(status_code=403, detail='AlreadyUsedProject')
+        
         # 해당 프로젝트 참여 인원 모두 삭제
         delete_project_members_all(db, project_code)
         # 프로젝트 삭제
